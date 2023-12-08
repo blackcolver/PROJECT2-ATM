5624ebd046c3fdfaa035ba6150dcd175be7c061b
Rakesh <104633038+blackcolver@users.noreply.github.com>
2023-12-08 20:23:45 +0530

    ATM.java

--- rakesh.java
+++ rakesh.java
@@ -1,92 +1,85 @@
-
 import java.util.*;
+
 class rakesh {
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-         String name,Password = "984868";
+        // Declare variables
+        String name, password = "984868";
+        long balance = 0, withdraw, deposit;
 
-         //declare and initialize balance, withdraw, and deposit  
-           long balance = 0 , withdraw,deposit;
+        // Create a Scanner class object to get details of the user
+        Scanner sc = new Scanner(System.in);
 
-         //create scanner class object to get details of user  
-         Scanner sc = new Scanner(System.in);
+        // Get user's name
+        System.out.print("Please Enter Your Name :");
+        name = sc.nextLine();
 
-         System.out.print("Please Enter Your Name :");
+        // Get user's PIN
+        System.out.print(" Hello " + name + " Please Enter Your PIN :");
+        password = sc.nextLine();
 
-         name = sc.nextLine();
-
-         System.out.print(" Hello " + name + " Please Enter Your PIN :");
-
-         Password = sc.nextLine();
-
-         switch(Password){
+        // Switch based on the entered PIN
+        switch (password) {
             case "984868":
-        
-           
-         while(true)  
-         {  
-             System.out.println("ATM");  
-             System.out.println("Choose 1 for Withdraw");  
-             System.out.println("Choose 2 for Deposit");  
-             System.out.println("Choose 3 for Check Balance");  
-             System.out.println("Choose 4 for EXIT");  
-             System.out.print("Choose the operation you want to perform:");  
-               
-             //get choice from user  
-             int choice = sc.nextInt();  
-             switch(choice)  
-             {  
-                 case 1:  
-         System.out.print("Enter money to be withdrawn:");  
-                       
-         //get the withdraw money from user  
-         withdraw = sc.nextInt();  
-                       
-         //check whether the balance is greater than or equal to the withdraw amount  
-         if(balance >= withdraw)  
-         {  
-             //remove the withdraw amount from the total balance  
-             balance = balance - withdraw;  
-             System.out.println("Please collect your money");  
-         }  
-         else  
-         {  
-             //show custom error message   
-             System.out.println("Insufficient Balance");  
-         }  
-         System.out.println("");  
-         break;  
-    
-                 case 2:  
-                       
-         System.out.print("Enter money to be deposited:");  
-                       
-         //get deposite amount from to user  
-         deposit = sc.nextInt();  
-                       
-         //add the deposit amount to the total balance  
-         balance = balance + deposit;  
-         System.out.println("Your Money has been successfully deposited");  
-         System.out.println("");  
-         break;  
-    
-                 case 3:  
-         //displaying the total balance of the user  
-         System.out.println("Balance : "+balance);  
-         System.out.println("");  
-         break;  
-    
-                 case 4:  
-          System.out.println("I Hope You Have Great Day ThankYou For Choosing Us !!");  
-             }  
-         }  
 
-                  //displaying the Wrong password of the user  
+                // ATM operations loop
+                while (true) {
+                    System.out.println("ATM");
+                    System.out.println("Choose 1 for Withdraw");
+                    System.out.println("Choose 2 for Deposit");
+                    System.out.println("Choose 3 for Check Balance");
+                    System.out.println("Choose 4 for EXIT");
+                    System.out.print("Choose the operation you want to perform:");
 
-         default:
-         System.out.println(" Your Password is Invalid " +name+ " Try Again!! ");
-         System.out.println();
-         break;
-     }  
+                    // Get choice from the user
+                    int choice = sc.nextInt();
+
+                    // Switch based on the user's choice
+                    switch (choice) {
+                        case 1:
+                            // Withdraw operation
+                            System.out.print("Enter money to be withdrawn:");
+                            withdraw = sc.nextInt();
+
+                            // Check if the balance is sufficient for withdrawal
+                            if (balance >= withdraw) {
+                                balance = balance - withdraw;
+                                System.out.println("Please collect your money");
+                            } else {
+                                System.out.println("Insufficient Balance");
+                            }
+                            System.out.println("");
+                            break;
+
+                        case 2:
+                            // Deposit operation
+                            System.out.print("Enter money to be deposited:");
+                            deposit = sc.nextInt();
+                            balance = balance + deposit;
+                            System.out.println("Your Money has been successfully deposited");
+                            System.out.println("");
+                            break;
+
+                        case 3:
+                            // Check balance operation
+                            System.out.println("Balance : " + balance);
+                            System.out.println("");
+                            break;
+
+                        case 4:
+                            // Exit operation
+                            System.out.println("I Hope You Have a Great Day! Thank You For Choosing Us!!");
+                            // Exit the program
+                            System.exit(0);
+                            break;
+                    }
+                }
+
+            // Display an invalid password message
+            default:
+                System.out.println("Your Password is Invalid " + name + " Try Again!!");
+                System.out.println();
+                break;
+        }
     }
 }
